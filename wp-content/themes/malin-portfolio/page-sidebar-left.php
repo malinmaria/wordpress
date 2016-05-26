c1a046a4aa79e78c492a55bb8d46a46fdd7c040c
Malin Nemergut <mmheineking@gmail.com>
2016-05-26 13:51:00 -0400

    adding a page and sidebar template

---
+++ wp-content/themes/malin-portfolio/page-sidebar-left.php
@@ -0,0 +1,35 @@
+<?php
+/*
+    Template Name: Left sidebar
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<section class="two-column row no-max pad">
+  <div class="small-12 columns">
+    <div class="row">
+      <!-- Primary Column -->
+      <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+        <div class="primary">
+
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <h1><?php the_title(); ?></h1>
+          <p><?php the_content(); ?></p>
+
+        <?php endwhile; else : ?>
+          <p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
+        <?php endif; ?>
+        </div>
+      </div>
+  <!-- Secondary Column -->
+    <div class="small-12 medium-4 medium-pull-8 columns">
+      <div class="secondary">
+        <h2 class="module-heading">Sidebar</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
