d986bab1e238a8e4ada1aef3ac21fab6fd951985
Malin Nemergut <mmheineking@gmail.com>
2016-05-26 12:57:18 -0400

    fixed typo and added content to index file

--- wp-content/themes/malin-portfolio/index.php
+++ wp-content/themes/malin-portfolio/index.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-<h1>Index file</h1>
+<section class="row">
+  <div class="small-12 columns text-center">
+    <div class="leader">
+      <h1>Etiam Mollis Sit Gratt Spore.</h1>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue.</p>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
