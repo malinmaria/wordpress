75187f64d8575f520f4eae4e2a76f377ea4b5521
Malin Nemergut <mmheineking@gmail.com>
2016-06-06 15:48:12 -0400

    building out blog page, adding title and excerpt php functions

--- wp-content/themes/malin-portfolio/home.php
+++ wp-content/themes/malin-portfolio/home.php
@@ -1,9 +1,3 @@
-<?php
-/*
-    Template Name: Left sidebar
-*/
- ?>
-
 <?php get_header(); ?>
 
 <section class="two-column row no-max pad">
@@ -15,8 +9,25 @@
 
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <h1><?php the_title(); ?></h1>
-          <p><?php the_content(); ?></p>
+            <article class="post">
+              <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              <h2><?php the_excerpt(); ?></h2>
+              <ul class="post-meta no-bullet">
+                <li class="author">
+                  <a href="author.html">
+                    <span class="avatar small">
+                      <img src="assets/img/avatar.png"/>
+                    </span>
+                    by Gratt Spore
+                  </a>
+                </li>
+                <li class="cat">in <a href="">Category 1</a></li>
+              </ul>
+              <div class="img-container">
+                <img src="assets/img/temp/item-06.png" alt="">
+                <p>Photo by Gratt Spore</p>
+              </div>
+            </article>
 
         <?php endwhile; else : ?>
           <p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
