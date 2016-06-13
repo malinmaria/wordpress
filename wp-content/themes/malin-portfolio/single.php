69af37b11fdbc7369c50c20ef18b47535d7c528f
Malin Nemergut <mmheineking@gmail.com>
2016-06-13 19:15:14 -0400

    widget functionality added and made sidebar function accesible from one file

--- wp-content/themes/malin-portfolio/single.php
+++ wp-content/themes/malin-portfolio/single.php
@@ -39,12 +39,9 @@
         <?php endif; ?>
         </div>
       </div>
-  <!-- Secondary Column -->
-    <div class="small-12 medium-4 medium-pull-8 columns">
-      <div class="secondary">
-        <h2 class="module-heading">Sidebar</h2>
-      </div>
-    </div>
+
+  <?php get_sidebar(); ?>
+
   </div>
 </section>
 
